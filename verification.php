3d584f81b47ad32e1e4a0e7f7ad067fc571e9eaa
Ethan Caillot <ethan.caillot@gmail.com>
2021-09-30 17:02:36 +0200

    oui

--- verification.php
+++ verification.php
@@ -35,10 +35,12 @@ $dbh = new PDO('mysql:host=localhost;dbname=test', $user, $pass, array(
 session_start();
 if(isset($_POST['username']) && isset($_POST['password']))
 {
+    
+
     // connexion à la base de données
-    $db_username = 'root';
-    $db_password = 'mot_de_passe_bdd';
-    $db_name     = 'nom_bdd';
+    $db_username = 'sam';
+    $db_password = 'dino';
+    $db_name     = 'acuponcture';
     $db_host     = 'localhost';
     $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
            or die('could not connect to database');
